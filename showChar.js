39f22352e7eece611c520986b63c2c620ea33d56
D-DF <daviddefreitas0192@gmail.com>
2021-02-16 16:43:06 +0100

    first commit

---
+++ showChar.js
@@ -0,0 +1,11 @@
+let txt = "C'était à Mégara, faubourg de Carthage, dans les jardins d'Hamilcar.";
+
+for(let i = 0; i < txt.length; i++) {
+  console.log(`Voici le caractère : ${txt[i]} et voici l'index : ${i}`)
+}
+
+let auteur = "C'est une citation de Gustave Flaubert, du livre Salammbô";
+
+for (let i = 1; i < auteur.length; i++) {
+  console.log(`Voici le nombre de caractère que contient cette phrase : ${i}`)
+}
